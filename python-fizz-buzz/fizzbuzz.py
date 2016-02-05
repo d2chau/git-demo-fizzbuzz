94cdb69545407c7b664780aef247be95540453eb
David Chau <chaudavid92@gmail.com>
2016-02-04 21:50:18 -0700

    Implement python fizzbuzz with dictionary

--- python-fizz-buzz/fizzbuzz.py
+++ python-fizz-buzz/fizzbuzz.py
@@ -1,4 +1,4 @@
-def fizzbuzz(n):
+def basicfizzbuzz(n):
 
     if n % 3 == 0 and n % 5 == 0:
         return 'FizzBuzz'
@@ -8,5 +8,29 @@ def fizzbuzz(n):
         return 'Buzz'
     else:
         return str(n)
+
+print "\n".join(basicfizzbuzz(n) for n in xrange(1, 100))
+
+print "\n"
+print "****************************************************************************"
+print "****************************************************************************"
+print "****************************************************************************"
+print "****************************************************************************"
+print "****************************************************************************"
+print "****************************************************************************"
+print "\n"
+
+def fizzbuzz(n):
+	one = False
+	tempStr = ""
+	dict = {'Buzz': 5, 'Fizz': 3};
+
+	for key, value in dict.items():
+		if n % value == 0:
+			tempStr = tempStr + key
+
+	if not tempStr:
+		tempStr = str(n)
+	return tempStr
 
 print "\n".join(fizzbuzz(n) for n in xrange(1, 100))
