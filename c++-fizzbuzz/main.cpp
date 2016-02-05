5aef8d964c04555aa887983bcc7d08b7a4cd73cf
David Chau <david.chau@mc02ll4lbfh00.readytalk.com>
2016-02-05 12:11:47 -0700

    Add remove std

--- c++-fizzbuzz/main.cpp
+++ c++-fizzbuzz/main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
-using namespace std;
+
 int main ()
 {
   for(int i = 1; i <= 100; i++)
   {
     if(i % 3 == 0 && i % 5 == 0)
-      cout << "FizzBuzz" << endl;
+      std::cout << "FizzBuzz" << '\n';
     else
     if(i % 3 == 0)
-      cout << "Fizz" << endl;
+      std::cout << "Fizz" << '\n';
     else
     if(i % 5 == 0)
-      cout << "Buzz" << endl;
-    else cout << i << endl;
+      std::cout << "Buzz" << '\n';
+    else std::cout << i << "\n\t\n";
   }
   return 0;
 }
